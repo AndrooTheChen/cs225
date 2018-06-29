bea8bd3611ceaebedc88a07500158508f76b1b44
Andrew Chen <andrew6@linux-a2.ews.illinois.edu>
2018-06-29 17:06:27 -0500

    ad;lfk

--- mp1/mp1.cpp
+++ mp1/mp1.cpp
@@ -7,6 +7,28 @@ using namespace cs225;
 
 void rotate(std::string inputFile, std::string outputFile) {
   // Part 2
+  PNG * inFile = NULL;
+
+  inFile->readFromFile(inputFile);
+  unsigned width = inFile->width();
+  unsigned height = inFile->height();
+
+  PNG * outFile = new PNG(width, height);
+
+  int newX, newY;
+  for(int x = 0; x < width; x++) {
+    for(int y = 0; y < height; y++) {
+      HSLAPixel * currInPixel = &(inFile->getPixel(x, y));
+      
+      newX = width - x;
+      newY = height - y;
+
+      HSLAPixel * currOutPixel = &(outFile->getPixel(newX, newY));
+    }
+  }
+
+  outFile->writeToFile(outputFile);
+
 }
 
 PNG myArt(unsigned int width, unsigned int height) {
