ec350b684e71e81e32ae70a4b1ce043a0f69c8f3
AndrooTheChen <achen226@gmail.com>
2018-07-26 17:24:33 -0700

    asdlf;j

--- mp2/Image.cpp
+++ mp2/Image.cpp
@@ -3,9 +3,9 @@
 void Image::lighten() {
     for (unsigned int y = 0; y < this->height(); y++) {
         for (unsigned int x = 0; x < this->width(); x++) {
-            HSLAPixel & pixel = getPixel(x, y);
+            HSLAPixel & pixel = this->getPixel(x, y);
             if (pixel.l > .9) {
-                continue;
+                pixel.l = 1;
             } else {
                 pixel.l += .1;
             }
@@ -16,7 +16,7 @@ void Image::lighten() {
 void Image::lighten(double amount) {
     for (unsigned int y = 0; y < this->height(); y++) {
         for (unsigned int x = 0; x < this->width(); x++) {
-            HSLAPixel & pixel = getPixel(x, y);
+            HSLAPixel & pixel = this->getPixel(x, y);
             if (pixel.l+amount > 1) {
                 pixel.l = 1;
             } else {
@@ -24,4 +24,124 @@ void Image::lighten(double amount) {
             }
         }
     }
+}
+
+void Image::darken() {
+    for (unsigned int y = 0; y < this->height(); y++) {
+        for (unsigned int x = 0; x < this->width(); x++) {
+            HSLAPixel & pixel = this->getPixel(x, y);
+            if (pixel.l < .1 ) {
+                pixel.l = 0;
+            } else {
+                pixel.l -= .1;
+            }
+        }
+    }
+}
+
+void Image::darken(double amount) {
+    for (unsigned int y = 0; y < this->height(); y++) {
+        for (unsigned int x = 0; x < this->width(); x++) {
+            HSLAPixel & pixel = this->getPixel(x, y);
+            if (pixel.l-amount < 0) {
+                pixel.l = 0;
+            } else {
+                pixel.l -= amount;
+            }
+        }
+    }
+}
+
+void Image::saturate() {
+    for (unsigned int y = 0; y < this->height(); y++) {
+        for (unsigned int x = 0; x < this->width(); x++) {
+            HSLAPixel & pixel = this->getPixel(x, y);
+            if (pixel.s > .9) {
+                pixel.s = 1;
+            } else {
+                pixel.s += .1;
+            }
+        }
+    }
+}
+
+void Image::saturate(double amount) {
+    for (unsigned int y = 0; y < this->height(); y++) {
+        for (unsigned int x = 0; x < this->width(); x++) {
+            HSLAPixel & pixel = this->getPixel(x, y);
+            if (pixel.s+amount > 1) {
+                pixel.s = 1;
+            } else {
+                pixel.s += amount;
+            }
+        }
+    }
+}
+
+void Image::desaturate() {
+    for (unsigned int y = 0; y < this->height(); y++) {
+        for (unsigned int x = 0; x < this->width(); x++) {
+            HSLAPixel & pixel = this->getPixel(x, y);
+            if (pixel.s < .1 ) {
+                pixel.s = 0;
+            } else {
+                pixel.s -= .1;
+            }
+        }
+    }
+}
+
+void Image::desaturate(double amount) {
+    for (unsigned int y = 0; y < this->height(); y++) {
+        for (unsigned int x = 0; x < this->width(); x++) {
+            HSLAPixel & pixel = this->getPixel(x, y);
+            if (pixel.s-amount < 0) {
+                pixel.s = 0;
+            } else {
+                pixel.s -= amount;
+            }
+        }
+    }
+}
+
+void Image::grayscale() {
+    for (unsigned y = 0; y < this->height(); y++) {
+        for (unsigned x = 0; x < this->width(); x++) {
+            HSLAPixel & pixel = this->getPixel(x, y);
+            pixel.s = 0;
+        }
+    }
+}
+
+void Image::rotateColor(double degrees) {
+    for (unsigned y = 0; y < this->height(); y++) {
+        for (unsigned x = 0; x < this->width(); x++) {
+            HSLAPixel & pixel = this->getPixel(x, y);
+            pixel.h = ((int)(degrees+pixel.h)) % 361;
+        }
+    }
+}
+
+void Image::illinify() {
+    for (unsigned int y = 0; y < this->height(); y++) {
+        for (unsigned int x = 0; x < this->width(); x++) {
+            HSLAPixel & pixel = this->getPixel(x, y);
+            if (pixel.h >= 139 || pixel.h < 294) {
+                pixel.h = 216;
+            } else {
+                pixel.h = 11;
+            }
+        }
+    }
+}
+
+void Image::scale(double factor) {
+    if (factor == 1.0) return;
+    int currWidth = this->width();
+    int currHeight = this->height();
+    this->resize(factor*currWidth, factor*currHeight);
+}
+
+void Image::scale(unsigned w, unsigned h) {
+    this->resize(w, h);
 }
--- mp2/Image.h
+++ mp2/Image.h
@@ -19,6 +19,8 @@ public:
     void darken (double amount);
     void saturate();
     void saturate(double amount);
+    void desaturate();
+    void desaturate(double amount);
     void grayscale();
     void rotateColor(double degrees);
     void illinify();
